4dfc8adf89d2ff2a6de1d0ac02e9f71d3bfa82f3
Deptl <pateldeep312000@gmail.com>
2024-03-06 18:16:29 -0500

    Add project files.

---
+++ TriangleTest/Class1.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+
+namespace TriangleTest
+{
+    [TestFixture]
+    public class TriangleTestClass
+    {
+        [Test]
+        public static void EquilateralTriangle_Input10and10and10_OutputEquilateralTriangle()
+        {
+            //Arrange
+            int firstSide = 10;
+            int secondSide = 10;
+            int thirdSide = 10;
+
+            string expected = "Equilateral triangle";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide); 
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
