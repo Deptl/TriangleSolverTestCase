bbd77128a8633cde65394354849ed38100dda080
Deptl <pateldeep312000@gmail.com>
2024-03-06 18:25:32 -0500

    Tests for Invalid Input Triangle

--- TriangleTest/Class1.cs
+++ TriangleTest/Class1.cs
@@ -27,5 +27,90 @@ namespace TriangleTest
             //Assert
             Assert.AreEqual(actual, expected);
         }
+
+        [Test]
+        public static void ScaleneTriangle_Input15and19and30_OutputScaleneTriangle()
+        {
+            //Arrange
+            int firstSide = 15;
+            int secondSide = 19;
+            int thirdSide = 30;
+
+            string expected = "Scalene triangle";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public static void ScaleneTriangle_Input5and18and16_OutputScaleneTriangle()
+        {
+            //Arrange
+            int firstSide = 5;
+            int secondSide = 18;
+            int thirdSide = 16;
+
+            string expected = "Scalene triangle";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public static void ScaleneTriangle_Input30and40and50_OutputScaleneTriangle()
+        {
+            //Arrange
+            int firstSide = 30;
+            int secondSide = 40;
+            int thirdSide = 50;
+
+            string expected = "Scalene triangle";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public static void ScaleneTriangle_Input14and27and19_OutputScaleneTriangle()
+        {
+            //Arrange
+            int firstSide = 14;
+            int secondSide = 27;
+            int thirdSide = 19;
+
+            string expected = "Scalene triangle";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public static void ScaleneTriangle_Input16and33and25_OutputScaleneTriangle()
+        {
+            //Arrange
+            int firstSide = 16;
+            int secondSide = 33;
+            int thirdSide = 25;
+
+            string expected = "Scalene triangle";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
     }
 }
